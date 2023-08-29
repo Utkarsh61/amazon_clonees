e051cf0039b326e209b5af078317a2a6915fed82
Utkarsh Tripathi <98511619+Utkarsh61@users.noreply.github.com>
2023-08-29 21:48:31 +0530

    Add files via upload

---
+++ src/components/cart/Cart.js
@@ -0,0 +1,137 @@
+import React, { useEffect, useState } from "react";
+import "./cart.css";
+import { Divider } from "@mui/material";
+import { useParams } from "react-router-dom";
+
+const Cart = () => {
+  const { id } = useParams("");
+
+  const [inddata, setInddata] = useState();
+  // console.log("inddata",inddata);
+
+  const getinddata = async () => {
+    const res = await fetch(`http://localhost:8005/getproductsone/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await res.json();
+
+    if (res.status !== 201) {
+      console.log("no data available");
+    } else {
+      console.log("getdata");
+      setInddata(data);
+    }
+  };
+
+  useEffect(() => {
+    getinddata();
+  }, [id]);
+
+  //add cart data
+
+  const addtocart = async (id) => {
+    try {
+      const checkres = await fetch(`http://localhost:8005/addcart/${id}`, {
+        method: "POST",
+        headers: {
+          "access-control-allow-origin" : "*",
+          "Content-type": "application/json; charset=UTF-8"
+        },
+        body: JSON.stringify({
+          inddata,
+        }),
+        credentials: "include",
+      });
+
+      const data1 = await checkres.json();
+      console.log(data1 + "frontend data");
+
+      if (checkres === 401 || !data1) {
+        console.log("User Invalid");
+        alert("User Invalid");
+      } else {
+        alert("data added in your cart");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="cart_section">
+      {inddata && Object.keys(inddata).length && (
+        <div className="cart_container">
+          <div className="left_cart">
+            <img src={inddata.url} alt="cart_img" />
+            <div className="cart_btn">
+              <button
+                className="cart_btn1"
+                onClick={() => addtocart(inddata.id)}
+              >
+                Add to Cart
+              </button>
+              <button className="cart_btn2">Buy Now</button>
+            </div>
+          </div>
+          <div className="right_cart">
+            <h3>{inddata.title.shortTitle}</h3>
+            {<h4>{inddata.title.longTitle}</h4>}
+            <Divider />
+            <p className="mrp">M.R.P. :₹{inddata.price.mrp}</p>
+            <p>
+              {" "}
+              Deal of the Day :{" "}
+              <span style={{ color: "#B12704" }}>₹{inddata.price.cost}</span>
+            </p>
+            <p>
+              {" "}
+              You save :{" "}
+              <span style={{ color: "#B12704" }}>
+                {inddata.price.mrp - inddata.price.cost}(
+                {inddata.price.discount})
+              </span>
+            </p>
+
+            <div className="discount_box">
+              <h5>
+                Discount :
+                <span style={{ color: "#111" }}>{inddata.discount}</span>
+              </h5>
+              <h4>
+                Free Delievery :
+                <span style={{ color: "#111", fontWeight: 600 }}>Oct 8-21</span>{" "}
+                Details
+              </h4>
+              <p>
+                Fastest delievery :
+                <span style={{ color: "#111", fontWeight: 600 }}>
+                  Tomarrow 11AM
+                </span>
+              </p>
+            </div>
+
+            <p className="description">
+              About the Item :{" "}
+              <span
+                style={{
+                  color: "#565959",
+                  fontSize: 14,
+                  fontWeight: 500,
+                  letterSpacing: "0.4px",
+                }}
+              >
+                {inddata.description}
+              </span>
+            </p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Cart;
